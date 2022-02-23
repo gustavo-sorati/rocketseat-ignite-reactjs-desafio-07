dba9d88f5262810486f53f5321ec99672a904af6
Gustavo Sorati <gustavo.sorati@outlook.com>
2022-02-23 01:21:52 -0300

    Base do projeto construida

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,11 @@
 import { Flex, Heading } from "@chakra-ui/react";
-import { GetStaticProps } from "next";
-import { api } from "../api/api";
 import Banner from "../components/Banner";
 import Caracteristicas from "../components/Caracteristicas";
 import Header from "../components/Header";
 import Separador from "../components/Separador";
 import Slider from "../components/Slider";
-import { ContinentData } from "../types/continent";
 
-export default function Home(continents: ContinentData[]) {
+export default function Home() {
   return (
     <Flex direction={"column"}>
       <Header />
@@ -29,14 +26,4 @@ export default function Home(continents: ContinentData[]) {
       <Slider />
     </Flex>
   )
-}
-
-export const getStaticProps: GetStaticProps = async (context) => {
-  const response = await api.get<ContinentData>();
-
-  return {
-    props: {
-      continent: [...response.data]
-    }
-  }
 }
